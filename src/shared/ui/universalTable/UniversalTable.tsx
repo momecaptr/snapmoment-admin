0b692f7899877c9070418d36d3380407e434b985
Daniil <daniilminin1990@yandex.ru>
2024-11-21 17:28:51 +0500

    minor refactor

--- src/shared/ui/universalTable/UniversalTable.tsx
+++ src/shared/ui/universalTable/UniversalTable.tsx
@@ -4,7 +4,6 @@ import { clsx } from 'clsx';
 
 import s from './UniversalTable.module.scss';
 import {TableParts, Typography} from "@momecap/ui-kit-snapmoment";
-import {AdditionalDataSingleObj} from "@/pagesComponents/usersList/UsersList";
 import {EmptyTable} from "@/shared/ui/universalTable/ui/EmptyTable";
 import {Loading} from "@/shared/ui";
 import {useQueryParams} from "@/shared/lib/hooks/useQueryParams";
