f70ddf23a6034e8c0b7bea49c16625e683231319
Daniil <daniilminin1990@yandex.ru>
2024-12-06 18:58:38 +0500

    failed to optimize UserCard

--- src/entities/userCard/UserCard.tsx
+++ src/entities/userCard/UserCard.tsx
@@ -1,8 +1,9 @@
-"use client"
-import {useState} from "react";
+"use client";
+
+import React, { useState, memo } from "react";
 import Image from "next/image";
-import Stub from '@/../public/epicpen_6ymMwEsBEI.png';
-import s from './UserCard.module.scss'
+import Stub from "@/../public/epicpen_6ymMwEsBEI.png";
+import s from "./UserCard.module.scss";
 import { Post } from "@/graphql/types";
 import { clsx } from "clsx";
 import { Block, Button, Typography } from "@momecap/ui-kit-snapmoment";
@@ -12,32 +13,26 @@ import { useGetAccessKeyFromStorage } from "@/shared/lib/hooks/useGetAccessKeyFr
 
 type Props = {
   post: Post;
-  openModalHandler: (value: string) => void
+  openModalHandler: (value: string) => void;
 };
 
-export const UserCard = (props: Props) => {
-  const { post, openModalHandler } = props
+export const UserCard = memo((props: Props) => {
+  const { post, openModalHandler } = props;
   const [isShowText, setIsShowText] = useState(false);
-  const accessKey = useGetAccessKeyFromStorage() ?? localStorage.getItem('accessKey')
-  console.log('USER_CARD')
+  const accessKey = useGetAccessKeyFromStorage() ?? localStorage.getItem("accessKey");
 
   const toggleShowText = () => setIsShowText(!isShowText);
 
   const { data: oneUser } = useGetOneUserQuery({
-    variables: {
-      userId: post.postOwner.id
-    },
-    context: {
-      base64UsernamePassword: accessKey
-    },
-  })
+    variables: { userId: post.postOwner.id },
+    context: { base64UsernamePassword: accessKey },
+  });
 
-  const isUserBanned = !!oneUser?.getUser.userBan?.reason
+  const isUserBanned = !!oneUser?.getUser.userBan?.reason;
 
   const clickHandler = () => {
-    console.log({banReason: isUserBanned, postOwnerId: post.postOwner.id, data: oneUser })
-    openModalHandler(isUserBanned ? 'unban' : 'ban' )
-  }
+    openModalHandler(isUserBanned ? "unban" : "ban");
+  };
 
   return (
     <div className={s.card}>
@@ -49,32 +44,34 @@ export const UserCard = (props: Props) => {
         <div className={s.authorContainer}>
           <div className={s.author}>
             <Image
-              alt={'author avatar'}
+              alt={"author avatar"}
               className={s.authorPhoto}
               height={100}
               src={post.postOwner.avatars?.[1]?.url || Stub}
               width={100}
               unoptimized
             />
-            <Typography variant={'h3'}>{post.postOwner.userName}</Typography>
+            <Typography variant={"h3"}>{post.postOwner.userName}</Typography>
           </div>
-          <Button className={s.blockBtn} onClick={clickHandler} >
-            <Block className={clsx(s.blockIcon, isUserBanned && s.banned)}/>
+          <Button className={s.blockBtn} onClick={clickHandler}>
+            <Block className={clsx(s.blockIcon, isUserBanned && s.banned)} />
           </Button>
         </div>
 
-        <TimeAgo time={post.createdAt as string}/>
+        <TimeAgo time={post.createdAt as string} />
 
-        <Typography as={'p'} className={clsx(s.description, isShowText && s.expanded)} variant={'regular_text_14'}>
+        <Typography as={"p"} className={clsx(s.description, isShowText && s.expanded)} variant={"regular_text_14"}>
           {post.description}
         </Typography>
 
         {post.description.length > 100 && (
           <Button className={s.showBtn} onClick={toggleShowText}>
-            {isShowText ? 'Hide' : 'Show more'}
+            {isShowText ? "Hide" : "Show more"}
           </Button>
         )}
       </div>
     </div>
   );
-};
+});
+
+UserCard.displayName = "UserCard";
--- src/widget/posts/Posts.tsx
+++ src/widget/posts/Posts.tsx
@@ -1,47 +1,40 @@
-"use client"
-import * as React from 'react';
-import {ChangeEvent, MutableRefObject, useCallback, useEffect, useRef, useState} from "react";
-import {IUseInfiniteScroll, ModalKey, useInfiniteScroll, useModal, useQueryParams} from "@/shared/lib";
+"use client";
+
+import React, { ChangeEvent, MutableRefObject, useCallback, useEffect, useRef, useState } from "react";
+import { IUseInfiniteScroll, ModalKey, useInfiniteScroll, useModal, useQueryParams } from "@/shared/lib";
 import {
   GetAllPostsQuery,
   useGetAllPostsLazyQuery,
-  useGetAllPostsQuery
 } from "@/graphql/queries/posts/getAllPosts.generated";
-import s from './Posts.module.scss'
-import {UserCard} from "@/entities/userCard/UserCard";
-import {useGetAccessKeyFromStorage} from "@/shared/lib/hooks/useGetAccessKeyFromStorage";
-import {Post, SortDirection} from "@/graphql/types";
-import {usePostAddedSubscription} from "@/graphql/subscriptions/postAdded.generated";
-import {BanUserModal, UnbanUserModal} from "@/features";
-import {Input} from "@momecap/ui-kit-snapmoment";
-
-type Props = {
-  data?: any
-};
+import s from "./Posts.module.scss";
+import { UserCard } from "@/entities/userCard/UserCard";
+import { useGetAccessKeyFromStorage } from "@/shared/lib/hooks/useGetAccessKeyFromStorage";
+import { Post } from "@/graphql/types";
+import { usePostAddedSubscription } from "@/graphql/subscriptions/postAdded.generated";
+import { BanUserModal, UnbanUserModal } from "@/features";
+import { Input } from "@momecap/ui-kit-snapmoment";
 
 const START_POSTS_COUNT = 10;
 const NEXT_POSTS_COUNT = 10;
 
-export const Posts = (props: Props) => {
+export const Posts = () => {
   const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const accessKey = useGetAccessKeyFromStorage()
-  const { debouncedSearchValue, setSearchQuery, searchTerm } = useQueryParams()
+  const accessKey = useGetAccessKeyFromStorage();
+  const { debouncedSearchValue, setSearchQuery, searchTerm } = useQueryParams();
   const { isOpen: isBanUserModalOpen, setOpen: setIsBanUserModalOpen } = useModal(ModalKey.BanUser);
   const { isOpen: isUnBanUserModalOpen, setOpen: setIsUnBanUserModalOpen } = useModal(ModalKey.UnBanUser);
-  const [pickedInfo, setPickedInfo] = useState<{userId: number | undefined, userName: string | undefined}>({userId: undefined, userName: undefined})
+  const [pickedInfo, setPickedInfo] = useState<{ userId: number | undefined; userName: string | undefined }>({
+    userId: undefined,
+    userName: undefined,
+  });
 
-  const [getAllPosts, { data: fetchedData, loading, subscribeToMore, refetch }] = useGetAllPostsLazyQuery();
-  const [posts, setPosts] = useState<GetAllPostsQuery['getPosts']['items']>(fetchedData?.getPosts?.items || []); // Локальное состояние для списка постов
-  const [onePost, setOnePost] = useState<Post>({} as Post); // Локальное состояние для списка постов
+  const [getAllPosts, { data: fetchedData, loading }] = useGetAllPostsLazyQuery();
+  const [posts, setPosts] = useState<GetAllPostsQuery["getPosts"]["items"]>([]);
 
-  // Подписка на новые посты
-  // При получении данных нового поста через onData обновляем состояние, добавляя новый пост в начало списка состояния posts
-  const { data: subscriptionData } = usePostAddedSubscription();
 
-  // Первоначальная загрузка постов и при изменении searchTerm
   useEffect(() => {
     if (accessKey && debouncedSearchValue !== undefined) {
-      setPosts([]); // Очистить текущие посты перед загрузкой новых
+      setPosts([]);
       getAllPosts({
         variables: {
           pageSize: START_POSTS_COUNT,
@@ -53,7 +46,6 @@ export const Posts = (props: Props) => {
     }
   }, [accessKey, debouncedSearchValue, getAllPosts]);
 
-  // Проверка на отсутствие дополнительных постов
   const hasNoMorePosts = fetchedData?.getPosts.totalCount === posts?.length;
 
   const onLoadNextPosts = useCallback(() => {
@@ -62,19 +54,18 @@ export const Posts = (props: Props) => {
 
       getAllPosts({
         variables: { endCursorPostId: cursorID, pageSize: NEXT_POSTS_COUNT },
-        context: { base64UsernamePassword: accessKey }
+        context: { base64UsernamePassword: accessKey },
       });
     }
   }, [getAllPosts, hasNoMorePosts, loading, posts]);
 
   useInfiniteScroll({
     callBack: onLoadNextPosts,
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0.1,
-    triggerRef
+    triggerRef,
   } as IUseInfiniteScroll);
 
-  // Обновление состояния постов при приходе новых данных
   useEffect(() => {
     if (fetchedData?.getPosts?.items) {
       setPosts((prev) => {
@@ -82,50 +73,45 @@ export const Posts = (props: Props) => {
           (newPost) => !prev.some((post) => post.id === newPost.id)
         );
         return [...prev, ...newPosts];
-      });// Добавление новых постов к текущему состоянию
+      });
     }
   }, [fetchedData]);
 
-  // Обновление списка постов при получении новых данных из подписки
-  useEffect(() => {
-    if (subscriptionData?.postAdded) {
-      const newPost = subscriptionData.postAdded;
-      setPosts((prevPosts) => [newPost, ...prevPosts]);
-    }
-  }, [subscriptionData]);
+  const handlePickInfoAndOpenModal = useCallback(
+    ({ post, value }: { post: Post; value: string }) => {
+      setPickedInfo({ userId: post.postOwner.id, userName: post.postOwner.userName });
+      if (value === "ban") {
+        setIsBanUserModalOpen(true);
+      } else {
+        setIsUnBanUserModalOpen(true);
+      }
+    },
+    [setIsBanUserModalOpen, setIsUnBanUserModalOpen]
+  );
 
-  const handlePickInfoAndOpenModal = ({post, value}: {post: Post, value: string}) => {
-    console.log({post, value})
-    setOnePost(post)
-    if(value === 'ban') {
-      setIsBanUserModalOpen(true)
-      setPickedInfo({userId: post.postOwner.id, userName: post.postOwner.userName})
-    }
-    if(value === 'unban') {
-      setIsUnBanUserModalOpen(true)
-    }
-  }
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.currentTarget.value)
-  }
+  const handleSearchChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.currentTarget.value);
+  }, [setSearchQuery]);
 
   return (
     <>
-      <BanUserModal isOpen={isBanUserModalOpen} setOpen={setIsBanUserModalOpen} userId={pickedInfo.userId} pickedUserName={pickedInfo.userName}/>
-      <UnbanUserModal post={onePost} isOpen={isUnBanUserModalOpen} setOpen={setIsUnBanUserModalOpen} />
-      <div style={{marginBottom: '20px'}}>
-        <Input callback={setSearchQuery} onChange={handleSearchChange} type={'search'} currentValue={searchTerm}/>
+      <BanUserModal
+        isOpen={isBanUserModalOpen}
+        setOpen={setIsBanUserModalOpen}
+        userId={pickedInfo.userId}
+        pickedUserName={pickedInfo.userName}
+      />
+      <UnbanUserModal isOpen={isUnBanUserModalOpen} setOpen={setIsUnBanUserModalOpen} post={fetchedData?.getPosts.items.find((post) => post.postOwner.id === pickedInfo.userId) || {} as Post}/>
+      <div style={{ marginBottom: "20px" }}>
+        <Input callback={setSearchQuery} onChange={handleSearchChange} type={"search"} currentValue={searchTerm} />
       </div>
       <div className={s.container}>
         <div className={s.cards}>
           {posts.map((post) => (
-            <UserCard key={post.id} post={post} openModalHandler={(value: string) => handlePickInfoAndOpenModal({post, value})}/>
+            <UserCard key={post.id} post={post} openModalHandler={(value) => handlePickInfoAndOpenModal({ post, value })} />
           ))}
         </div>
-        <div ref={triggerRef} style={{opacity: 0}}>
-          .
-        </div>
+        <div ref={triggerRef} style={{ opacity: 0 }}>.</div>
       </div>
     </>
   );
